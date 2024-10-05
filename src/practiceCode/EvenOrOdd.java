73f219097e9e09a051f3f1237f605a977e9e4d6f
Hunter <hunter@gmail.com>
2024-10-05 15:37:06 +0530

    FactorialNumber

--- src/practiceCode/EvenOrOdd.java
+++ src/practiceCode/EvenOrOdd.java
@@ -4,6 +4,15 @@ public class EvenOrOdd {
 	
 	public static void main(String[] args) {
 		evenOdd();
+		factorial();
+	}
+
+	public static void factorial() {
+		int fact=1;
+		for (int i = 1; i <= 5; i++) {
+			fact=fact*i;
+		}
+		
 	}
 
 	public static void evenOdd() {
