2d3e3a470645e209c3a5f410330dfd8d86eaefef
Hunter <hunter@gmail.com>
2024-10-05 15:30:16 +0530

    checkingGitCommands

---
+++ src/practiceCode/EvenOrOdd.java
@@ -0,0 +1,19 @@
+package practiceCode;
+
+public class EvenOrOdd {
+	
+	public static void main(String[] args) {
+		evenOdd();
+	}
+
+	public static void evenOdd() {
+		
+		int a=10;
+		if(a%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+		
+	}
+
+}
